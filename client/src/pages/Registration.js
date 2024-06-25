e63cb6c6c9736384506dd202455cbe941a83e2dc
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-06-25 09:30:36 +0200

    rewrited gitifnore

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -1,25 +1,49 @@
-import React from "react";
+import React, {usrState,useEffect} from "react";
 import Button from "../components/Button";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 function Registration() {
+
+let nicknameInput = ()=>{
+
+}
+
   return (
     <div className="registration__wrapper">
-        <div className="container">
-          <h1>Get Started</h1>
-          <div className="form">
-            <form className="form__fields" action="">
-                <input className="form__input" placeholder="Nickname*" type="text" />
-                <input className="form__input" placeholder="Email*" type="email" />
-                <input className="form__input" placeholder="Password*" type="text" />
-                <input className="form__input" placeholder="Repeat Password*" type="text" />
-                <div className="terms">
-                    <input type="checkbox" />
-                    <p>By checking the box you agree to our <Link to="/terms">Terms and Conditions</Link></p>
-                </div>
-                <button><Button  content={"Continue"} /></button>
-                <p className="terms" >Already  a member? <Link to="/login"> Log In</Link></p>
-            </form>
-          </div>
+      <div className="container">
+        <h1>Get Started</h1>
+        <div className="form">
+          <form className="form__fields" action="">
+            <input
+              className="form__input"
+              placeholder="Nickname*"
+              type="text"
+            />
+            <input className="form__input" placeholder="Email*" type="email" />
+            <input
+              className="form__input"
+              placeholder="Password*"
+              type="text"
+            />
+            <input
+              className="form__input"
+              placeholder="Repeat Password*"
+              type="text"
+            />
+            <div className="terms">
+              <input type="checkbox" />
+              <p>
+                By checking the box you agree to our{" "}
+                <Link to="/terms">Terms and Conditions</Link>
+              </p>
+            </div>
+            <button onClick={e}>
+              <Button content={"Continue"} />
+            </button>
+            <p className="terms">
+              Already a member? <Link to="/login"> Log In</Link>
+            </p>
+          </form>
+        </div>
       </div>
     </div>
   );
