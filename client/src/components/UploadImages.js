32ae1dbf3f3611945880c7c0f4f5646ccd4a34e9
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-07-04 16:44:47 +0200

    env

--- client/src/components/UploadImages.js
+++ client/src/components/UploadImages.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import Button from "./Button";
 import { URL } from "../config";
 import { useContext } from "react";
-import { UserContext } from "../context/UserContext";
 import { PlacesContext } from "../context/PlacesContext";
 const UploadImages = ({
   content,
