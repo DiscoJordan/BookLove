7de3ae65fd4f13377117c87b72317b48f2b4111d
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-07-11 09:40:58 +0200

    build

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,7 @@
-const URL = `http://localhost:5050`;
+
+const URL =
+    window.location.hostname === `localhost`
+        ? `http://localhost:5050`
+        : `http://64.225.107.97`; 
 
 export { URL };
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 var cors = require("cors");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const app = express();
 app.use(cors());
 const port = process.env.port || 5050;
@@ -8,24 +8,28 @@ require("dotenv").config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose.set('debug',true)
+mongoose.set("debug", true);
 async function connecting() {
-    try {
-      await mongoose.connect(process.env.MONGO);
-      console.log("Connected to the Barcelove DB");
-    } catch (error) {
-      console.log(
-       error
-      );
-    }
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to the Barcelove DB");
+  } catch (error) {
+    console.log(error);
   }
+}
 
-  connecting();
-  app.use('/place', require('./routes/places'))
-  app.use('/user', require('./routes/users'))
-  app.use('/tag', require('./routes/tags'))
+connecting();
+app.use("/place", require("./routes/places"));
+app.use("/user", require("./routes/users"));
+app.use("/tag", require("./routes/tags"));
 
-  app.listen(port, () => console.log(`server listening on port ${port}`));
+//serving client
+const path = require("path");
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
 
-
+app.listen(port, () => console.log(`server listening on port ${port}`));
